5d28272ea2c8c871c852326d9f81b29e85a6f01d
codingkitty81 <codingkitty81@gmail.com>
2016-02-01 23:14:17 +0000

    restaurant.cc

---
+++ Project1/restaurant.cc
@@ -0,0 +1,16 @@
+
+#include "restaurant.h"
+
+Restaurant::getInput() {
+   
+}
+
+Restaurant::serveParties() {
+   
+}
+
+int Main(int argc, char* argv[]) {
+   Restaurant myDiner;
+   myDiner.getInput();
+   myDiner.serveParties();
+}
