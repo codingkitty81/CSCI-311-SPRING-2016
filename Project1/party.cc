4a4f299bde599519347709723c4e2f13c140c88d
codingkitty81 <codingkitty81@gmail.com>
2016-02-05 22:30:51 +0000

    party.cc

--- Project1/party.cc
+++ Project1/party.cc
@@ -6,7 +6,11 @@ Party::Party() {
 }
 
 Party::Party(const string& reservationName, int numDiners, int timeRequired) {
+   std::string* tempRes = new string(reservationName);
    
+   this->reservationName = tempRes;
+   this->numDiners = numDiners;
+   this->timeRequired = timeRequired;
 }
 
 Party::~Party() {
