0b668e8982b007f54ebeebfdcc5368721c05e253
codingkitty81 <codingkitty81@gmail.com>
2016-02-18 20:36:39 +0000

    CensusDataSorts.cpp

---
+++ Project2/CensusDataSorts.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file CensusDataSorts.cpp   Sort functions for census population data.
+ * 
+ * @brief
+ *    Implements several different types of sorts. Data can be sorted
+ * by population or by name of town. This file contains all of the sorting
+ * functions and their helpers.
+ *
+ * @author Judy Challinger
+ * @date 2/22/13
+ */
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include "CensusData.h"
+
+// formal parameter name commented out to avoid unused variable warning
+void CensusData::insertionSort(int /*type*/) {
+}
+
+// formal parameter name commented out to avoid unused variable warning
+void CensusData::mergeSort(int /*type*/) {
+}
+
+// formal parameter name commented out to avoid unused variable warning
+void CensusData::quickSort(int /*type*/) {
+}
