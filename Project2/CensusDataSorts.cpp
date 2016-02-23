c6d7962378d308434881dc14ec1f65d74ce08f2a
codingkitty81 <codingkitty81@gmail.com>
2016-02-23 17:42:03 +0000

    CensusDataSorts.cpp

--- Project2/CensusDataSorts.cpp
+++ Project2/CensusDataSorts.cpp
@@ -17,11 +17,38 @@
 #include "CensusData.h"
 
 // formal parameter name commented out to avoid unused variable warning
-void CensusData::insertionSort(int /*type*/) {
+void CensusData::insertionSort(int type) {
+   int vecSize = this->data.size();
+   
+   if(type == 0) {
+      for(int j = 1; j < vecSize; j++) {
+         Record* key = this->data[j];
+         int i = j-1;
+            while(i > -1 && this->data[i]->population > key->population) {
+               data[i+1] = data[i];
+               i = i-1;
+            }
+         data[i+1] = key;
+      }
+   } else {
+      for(int j = 1; j < vecSize; j++) {
+         Record* key = this->data[j];
+         int i = j-1;
+            while(i > -1 && *(this->data[i]->city) > *(key->city)) {
+               data[i+1] = data[i];
+               i = i-1;
+            }
+         data[i+1] = key;
+      }
+   }
+   
 }
 
 // formal parameter name commented out to avoid unused variable warning
-void CensusData::mergeSort(int /*type*/) {
+void CensusData::mergeSort(int type) {
+   if(type == 0) {
+      
+   }
 }
 
 // formal parameter name commented out to avoid unused variable warning
