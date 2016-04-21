5c7496aac40f3cddf3fd75a7fe905450db3758e5
codingkitty81 <codingkitty81@gmail.com>
2016-04-21 05:12:37 +0000

    rbapp.cpp

--- Project4/rbapp.cpp
+++ Project4/rbapp.cpp
@@ -3,19 +3,72 @@
  */
  
 #include "rbapp.h"
+#include <sstream>
+#include <iostream>
 
-RBapp::RBapp() {}
+RBapp::RBapp() : done(false) {}
 
-void RBapp::mainLoop() {}
+void RBapp::mainLoop() {
+   while(done == false) {
+      processCommand();
+   }
+}
 
-void RBapp::processCommand() {}
+void RBapp::processCommand() {
+   //std::cout << "Enter a command: ";
+   std::string line;
+   getline(std::cin, line);
+   std::istringstream input(line);
+   std::string command;
+   input >> command;
+   line = line.substr(line.find_first_of(" \t")+1);
+   if(command == "insert") {
+      processInsert(line);
+   } else if(command == "print") {
+      processPrint();
+   } else if(command == "find") {
+      processFind(line);
+   } else if(command == "delete") {
+      processDelete(line);
+   } else if(command == "quit") {
+      processQuit();
+   } else {
+      done = true;
+   }
+}
 
-void RBapp::processInsert(string& key) {}
+void RBapp::processInsert(string& line) {
+   int pos = line.find(' ');
+   string key = line.substr(0, pos);
+   line = line.substr(line.find_first_of(" \t")+1);
+   myRBT.rbInsert(key, line);
+}
 
-void RBapp::processPrint() {}
+void RBapp::processPrint() {
+   myRBT.rbPrintTree();
+}
 
-void RBapp::processFind(string& key) {}
+void RBapp::processFind(string& key) {
+   vector<const string*> foundList = myRBT.rbFind(key);
+   for(vector<const string*>::iterator it = foundList.begin();
+         it != foundList.end(); it += 2) {
+            std::cout << *(*it) << " " << *(*it) << std::endl;
+         }
+}
 
-void RBapp::processDelete(string& key) {}
+void RBapp::processDelete(string& line) {
+   int pos = line.find(' ');
+   string key = line.substr(0, pos);
+   line = line.substr(line.find_first_of(" \t")+1);
+   myRBT.rbDelete(key, line);
+}
 
-void RBapp::processQuit() {}
+void RBapp::processQuit() {
+   done = true;
+}
+
+int main() {
+   RBapp* myRBapp = new RBapp();
+   myRBapp->mainLoop();
+   return 0;
+}
