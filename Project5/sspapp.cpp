b8e221ca453a9366007f4125268ac63697886c76
codingkitty81 <codingkitty81@gmail.com>
2016-05-13 00:23:17 +0000

    sspapp.cpp

--- Project5/sspapp.cpp
+++ Project5/sspapp.cpp
@@ -1,7 +1,23 @@
-
+/*
+ * @file sspapp.cpp Processes input to build and query a graph to
+ * find the shortest path
+ *
+ * @author Katherine Jouzapaitis
+ * @date 05/08/2016
+ */
 
 #include "sspapp.h"
 
+/*
+ * @brief Processes input to build the lists of vertices and
+ * weighted edges
+ *
+ * @return nothing
+ *
+ * Uses std cin to get input. First input the number of vertices,
+ * next a string with all the vertex names, next the number of
+ * weighted edges, then looped through to get the edges.
+ */
 void SSPapp::readGraph() {
     int vertNum;
     std::cin >> vertNum;
@@ -24,6 +40,15 @@ void SSPapp::readGraph() {
     }
 }
 
+/*
+ * @brief Processes the queries to calculate the shortest
+ * path between two given vertices.
+ * 
+ * @return nothing
+ *
+ * Using std cin for input. The line gets separated into
+ * two different strings to call getShortestPath.
+ */
 void SSPapp::processQueries() {
     string line;
     string from;
@@ -39,6 +64,5 @@ int main() {
     SSPapp app;
     app.readGraph();
     app.processQueries();
-    
     return 0;
 }
